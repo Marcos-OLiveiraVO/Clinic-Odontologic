25ff4c582b71ea25731bc9fda700bc2ec83e7f70
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-07-03 01:52:10 -0300

    test: profilePatients

--- src/modules/patients/useCases/showProfilePatients/showProfilePatientUseCase.spec.ts
+++ src/modules/patients/useCases/showProfilePatients/showProfilePatientUseCase.spec.ts
@@ -2,6 +2,7 @@ import { PatientRepositoryInMemory } from "modules/patients/repositories/In-Memo
 import { CreatePatientUseCase } from "../createPatient/createPatientUseCase";
 import { ShowProfilePatientUseCase } from "./showProfilePatientUseCase";
 import { beforeAll, describe, expect, it } from "vitest";
+import { AppError } from "@errors/appError";
 
 let patientRepository: PatientRepositoryInMemory;
 let createPatientUseCase: CreatePatientUseCase;
@@ -38,5 +39,18 @@ describe("Show Patient Profile", async () => {
     );
 
     expect(showPatientProfile).toEqual(patient);
+  });
+
+  it("should not be able to show profile if patients not exists", async () => {
+    const patient = {
+      name: "XXX",
+      email: "test@mail.com",
+      phone: "XXX-XXX",
+      insurance_id: 6666,
+    };
+
+    await expect(
+      showProfilePatientUseCase.execute(patient.email)
+    ).rejects.toEqual(new AppError("Patient or email not exists!"));
   });
 });
