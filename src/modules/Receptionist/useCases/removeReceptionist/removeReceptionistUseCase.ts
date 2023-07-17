1c68877d0c5b51159d75073112e4401d0c45bd09
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-07-17 01:24:45 -0300

    refactor: removeReceptionistUsecase

--- src/modules/Receptionist/useCases/removeReceptionist/removeReceptionistUseCase.ts
+++ src/modules/Receptionist/useCases/removeReceptionist/removeReceptionistUseCase.ts
@@ -5,9 +5,11 @@ class RemoveReceptionistUseCase {
   constructor(private receptionistRepository: IReceptionistRepository) {}
 
   async execute(email: string): Promise<void> {
-    const receptionist = await this.receptionistRepository.findByEmail(email);
+    const receptionistExists = await this.receptionistRepository.findByEmail(
+      email
+    );
 
-    if (!receptionist) {
+    if (!receptionistExists) {
       throw new AppError("Receptionist account or email not exists!");
     }
 
