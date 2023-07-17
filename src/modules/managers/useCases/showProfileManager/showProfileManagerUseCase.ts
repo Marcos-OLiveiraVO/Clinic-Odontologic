4db79a2a8524e03daede03df98442a836edcf83d
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-07-17 02:07:34 -0300

    refactor: showProfileManager

--- src/modules/managers/useCases/showProfileManager/showProfileManagerUseCase.ts
+++ src/modules/managers/useCases/showProfileManager/showProfileManagerUseCase.ts
@@ -1,19 +1,19 @@
 import { AppError } from "@errors/appError";
-import { IResponseManagerDTO } from "modules/managers/dtos/IResponseManagerDTO";
+import { IResponseManagerProfile } from "modules/managers/dtos/IResponseManagerProfile";
 import { ManagerMapper } from "modules/managers/mappers/ManagerMapper";
 import { IManagerRepository } from "modules/managers/repositories/IManagerRepository";
 
 class ShowProfileManagerUseCase {
   constructor(private managerRepository: IManagerRepository) {}
 
-  async execute(email: string): Promise<IResponseManagerDTO> {
-    const manager = await this.managerRepository.findByEmail(email);
+  async execute(email: string): Promise<IResponseManagerProfile> {
+    const managerExists = await this.managerRepository.findByEmail(email);
 
-    if (!manager) {
+    if (!managerExists) {
       throw new AppError("Manager account or email not exists!");
     }
 
-    return ManagerMapper.ToDTO(manager);
+    return ManagerMapper.ToDTO(managerExists);
   }
 }
 
