0c1cb48a4bf66e17ac239e82c90b61c7a80e570a
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-07-31 00:11:29 -0300

    refactor: authModule

--- src/shared/infra/http/middlewares/auth/nestModule/auth.module.ts
+++ src/shared/infra/http/middlewares/auth/nestModule/auth.module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 
-import { AdminRepository } from "modules/admin/infra/prisma/adminRepository";
 import { IAdminRepository } from "modules/admin/repositories/IAdminRepository";
 import { PrismaModule } from "../../../../prisma/prisma.module";
 import { AuthController } from "shared/infra/http/middlewares/auth/controllers/auth.controller";
 import { AuthService } from "../useCases/authAdmin/authUseCase";
-import { AuthGuard } from "../AuthGuard/AuthGuard";
-import { APP_GUARD } from "@nestjs/core";
+import { AdminRepository } from "modules/admin/infra/prisma/repositories/adminRepository";
 
 @Module({
   imports: [
