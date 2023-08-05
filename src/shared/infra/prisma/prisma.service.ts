abf311c48934f659d4598dafa27a783b62d8d06c
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-08-05 00:39:01 -0300

    revert

--- src/shared/infra/prisma/prisma.service.ts
+++ src/shared/infra/prisma/prisma.service.ts
@@ -4,16 +4,6 @@ import { PrismaClient } from "@prisma/client";
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.prisma.$connect();
-  }
-
-  async onModuleDestroy() {
-    try {
-      await this.prisma.$disconnect();
-    } catch (error) {
-      console.error("Error while disconnecting PrismaClient:", error);
-    }
+    await this.$connect();
   }
 }
-
-export { PrismaService };
