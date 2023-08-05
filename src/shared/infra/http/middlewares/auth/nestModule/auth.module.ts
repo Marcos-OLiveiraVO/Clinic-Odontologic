4f292a6bb6acb68e5dad53db0067d3a3ee9e4119
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-08-05 00:47:34 -0300

    Revert "refactor: authModule"
    This reverts commit 0c1cb48a4bf66e17ac239e82c90b61c7a80e570a.

--- src/shared/infra/http/middlewares/auth/nestModule/auth.module.ts
+++ src/shared/infra/http/middlewares/auth/nestModule/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { JwtModule, JwtService } from "@nestjs/jwt";
 
+import { AdminRepository } from "modules/admin/infra/prisma/adminRepository";
 import { IAdminRepository } from "modules/admin/repositories/IAdminRepository";
 import { PrismaModule } from "../../../../prisma/prisma.module";
 import { AuthController } from "shared/infra/http/middlewares/auth/controllers/auth.controller";
 import { AuthService } from "../useCases/authAdmin/authUseCase";
-import { AdminRepository } from "modules/admin/infra/prisma/repositories/adminRepository";
+import { AuthGuard } from "../AuthGuard/AuthGuard";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
   imports: [
