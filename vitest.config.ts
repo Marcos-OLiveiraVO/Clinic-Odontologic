56c721b9c35744a02c87fb2a7f6c7cfe7752ab5f
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-08-08 01:56:28 -0300

    refactor: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["html"],
     },
+    environmentMatchGlobs: [["src/**", "prisma"]],
   },
   plugins: [
     tsconfigPaths(),
